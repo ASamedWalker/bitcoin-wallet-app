63c07e2214427e67bb1d18fc256aabb9caa7a1cb
ASamedWalker <ASamedWalker@users.noreply.github.com>
2025-09-29 00:08:54 -0400

    Initial commit: Bitcoin wallet app with private key, public key, and address generation

---
+++ hello_bitcoin.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Title - Hello Bitcoin
+This program demonstrates the creation of 
+- private key
+- public key
+- and a bitcoin address.
+"""
+# import bitcoin
+from bitcoin import *
+
+my_private_key = random_key()
+
+print(f"Private Key: \n{my_private_key}")
+
+# Generate the public key from the private key
+my_public_key = privtopub(my_private_key)
+print(f"Public Key: \n{my_public_key}")
+
+# Generate a bitcoin address from the public key
+my_bitcoin_address = pubtoaddr(my_public_key)
+print(f"Bitcoin Address: \n{my_bitcoin_address}")
